2a06db834753cb7086a8878f27cfeb94d461d746
davobad <davobad@umich.edu>
2024-10-25 19:06:25 -0400

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -3,7 +3,8 @@
  * Project UID 2e6ea4e086ea6a06753e819c30923369
  *
  * EECS 183
- * Project 4: CoolPics
+ * Lab 7
+ * Taken from Project 4: CoolPics
  *
  * <#Name(s)#>
  * <#uniqname(s)#>
@@ -16,64 +17,55 @@
  // for the declaration of DIMENSION
 #include "utility.h"
 
-// TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
 
-class Point
-{
-public:
 
-    Point() {
-        x = 0;
-        y = 0;
+Point::Point() {
+    x = 0;
+    y = 0;
+}
+
+Point::Point(int xVal, int yVal) {
+    x = checkRange(xVal);
+    y = checkRange(yVal);
+}
+
+void Point::setX(int xVal) {
+    x = checkRange(xVal);
+}
+
+int Point::getX() {
+    return x;
+}
+void Point::setY(int yVal) {
+    y = checkRange(yVal);
+}
+
+int Point::getY() {
+    return y;
+}
+
+void Point::read(istream& ins) {
+    char junk;
+    ins >> junk >> x >> junk >> y >> junk;
+    x = checkRange(x);
+    y = checkRange(y);
+}
+
+void Point::write(ostream& outs) {
+    outs << "(" << x << "," << y << ")";
+}
+
+int Point::checkRange(int val) {
+    if (val >= 0 && val < DIMENSION) {
+        return val;
     }
-
-    Point(int xVal, int yVal) {
-        x = xVal;
-        y = yVal;
+    else if (val < 0) {
+        return 0;
     }
-
-    void setX(int xVal) {
-        x = xVal;
+    else {
+        return DIMENSION - 1;
     }
-
-    int getX() {
-        return x;
-    }
-
-    void setY(int yVal) {
-        y = yVal;
-    }
-
-    int getY() {
-        return y;
-    }
-
-    void read(istream& ins) {
-        ins;
-    }
-
-    void write(ostream& outs) {
-        outs << "(" << x << "," << y << ")";
-    }
-
-private:
-    int x;
-    int y;
-
-
-    int checkRange(int val) {
-        if (val >= 0 && val < DIMENSION) {
-            return val;
-        }
-        else if (0 - val < val - (DIMENSION - 1)) {
-            return 0;
-        }
-        else {
-            return DIMENSION - 1;
-        }
-    }
-
-};
+}
 
 // Your code goes above this line.
 // Don't change the implementations below!
